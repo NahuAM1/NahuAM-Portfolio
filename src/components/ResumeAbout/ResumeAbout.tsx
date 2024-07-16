7ad2b863805d31941929a8e09f472e2d4d019517
Nahuel Manero <80863427+NahuAM1@users.noreply.github.com>
2024-07-16 11:26:20 -0300

    Update ResumeAbout.tsx

--- src/components/ResumeAbout/ResumeAbout.tsx
+++ src/components/ResumeAbout/ResumeAbout.tsx
@@ -1,47 +1,54 @@
-import "./ResumeAbout.scss";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import IconButton from "../../ui-components/IconButton/IconButton.tsx";
-import ButtonTemplate from "../../ui-components/ButtonTemplate/ButtonTemplate.tsx";
-import CV from "../../shared/media/Curriculum Vitae - Manero Nahuel.pdf";
+import './ResumeAbout.scss';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import IconButton from '../../ui-components/IconButton/IconButton.tsx';
+import ButtonTemplate from '../../ui-components/ButtonTemplate/ButtonTemplate.tsx';
+import CV from '../../shared/media/Curriculum Vitae - Manero Nahuel.pdf';
 const ResumeAbout = () => {
   return (
-    <div className="resumeAbout-container">
-      <div className="resumeAbout-title">Acerca De Mi</div>
-      <div className="resumeAbout-resume">
-        <p className="resumeAbout-resume-title">
+    <div className='resumeAbout-container'>
+      <div className='resumeAbout-title'>Acerca De Mi</div>
+      <div className='resumeAbout-resume'>
+        <p className='resumeAbout-resume-title'>
           Dejame contarte un poco mas de mi:
         </p>
-        <p className="resumeAbout-resume-text">
+        <p className='resumeAbout-resume-text'>
           Soy una persona apasionada por el deporte, disfruto ver series y jugar
           a videojuegos. Amo pasar tiempo al aire libre, practicando pádel y
-          artes marciales. Viajar es una de mis grandes pasiones, y mi mayor
-          sueño es vivir en algun lugar de la Patagonia y trabajar de forma
-          remota desde allí.
+          artes marciales. Viajar es una de mis grandes pasiones, y mi lugar
+          favorito en el mungo es la Patagonia Argentina.
           <br />
           Me encantan los animales, la tecnología y siempre estoy al tanto de
           las últimas innovaciones.
           <br />
           Valoro mucho pasar tiempo con mis amigos y mi familia, ya que esos
           momentos compartidos son muy importantes para mí.
+          <br />
+          Profesionalmente, mi propósito es convertirme en un Ingeniero en
+          Sistemas de Información respetado y confiable, reconocido por mi
+          profesionalismo y habilidades técnicas. Aspiro a alcanzar la libertad
+          financiera, permitiéndome vivir cómodamente y sin preocupaciones
+          económicas, y utilizar mis conocimientos para resolver problemas
+          complejos y promover la innovación tecnológica de manera ética y
+          responsable.
         </p>
-        <div className="resumeAbout-buttons">
+        <div className='resumeAbout-buttons'>
           <a
             href={CV}
-            download="Curriculum Vitae - Manero Nahuel.pdf"
-            className="a"
+            download='Curriculum Vitae - Manero Nahuel.pdf'
+            className='a'
           >
-            <ButtonTemplate name="Descarga mi CV" />
+            <ButtonTemplate name='Descarga mi CV' />
           </a>
           <IconButton
             icon={<LinkedInIcon />}
-            href={"https://www.linkedin.com/in/nahuelmanero/"}
-            name={""}
+            href={'https://www.linkedin.com/in/nahuelmanero/'}
+            name={''}
           />
           <IconButton
             icon={<GitHubIcon />}
-            href={"https://github.com/NahuAM1"}
-            name={""}
+            href={'https://github.com/NahuAM1'}
+            name={''}
           />
         </div>
       </div>
