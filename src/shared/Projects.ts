8e8ace607ff39bc2eb20c48a8ff97c60302d7b74
Nahuel Manero <80863427+NahuAM1@users.noreply.github.com>
2024-07-16 11:00:22 -0300

    commit changes

--- src/shared/Projects.ts
+++ src/shared/Projects.ts
@@ -1,28 +1,37 @@
-export interface Project{
-    name: string,
-    description: string,
-    image: string,
-    year: string,
-    role: string,
-    technologies: string,
+export interface Project {
+  name: string;
+  description: string;
+  image: string;
+  year: string;
+  role: string;
+  technologies: string;
+  demo: string;
+  repository: string;
 }
 
 const projects: Project[] = [
-    {
-        name: "Coffee Listing",
-        description: "Este projecto es un catalago de cafés el cual consume los datos de una API y muestra los cafés populares, los puntajes dados por la gente, los disponibes, etc.",
-        image: "https://i.postimg.cc/25z82yxw/Sin-t-tulo.png",
-        year: "2024",
-        role: "Desarrollador Front-End",
-        technologies: "TypeScript, React, Sass",
-    },{
-        name: "Gym App",
-        description: "Este projecto es una aplicacion que permite la gestion de clientes, pases y empleados de un gimnasio",
-        image: "https://i.postimg.cc/DyXVsh1w/Sin-t-tulo.png",
-        year: "2023",
-        role: "Desarrollador Full-Stack",
-        technologies: "JavaScript, React, Java-SpringBoot",
-    },
-]
+  {
+    name: 'Coffee Listing',
+    description:
+      'Este projecto es un catalago de cafés el cual consume los datos de una API y muestra los cafés populares, los puntajes dados por la gente, los disponibes, etc.',
+    image: 'https://i.postimg.cc/25z82yxw/Sin-t-tulo.png',
+    year: '2024',
+    role: 'Desarrollador Front-End',
+    technologies: 'TypeScript, React, Sass',
+    demo: 'https://simple-coffee-listing-pied.vercel.app',
+    repository: 'https://github.com/NahuAM1/Simple-Coffee-Listing',
+  },
+  {
+    name: 'Gym App',
+    description:
+      'Este projecto es una aplicacion que permite la gestion de clientes, pases y empleados de un gimnasio',
+    image: 'https://i.postimg.cc/DyXVsh1w/Sin-t-tulo.png',
+    year: '2023',
+    role: 'Desarrollador Full-Stack',
+    technologies: 'JavaScript, React, Java-SpringBoot',
+    demo: '',
+    repository: 'https://github.com/NahuAM1/gymapp',
+  },
+];
 
-export{ projects }
+export { projects };
--- src/ui-components/ProjectCard/ProjectCard.tsx
+++ src/ui-components/ProjectCard/ProjectCard.tsx
@@ -1,8 +1,8 @@
-import "./ProjectCard.scss";
-import TextButton from "../TextButton/TextButton";
-import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import { Project } from "../../shared/Projects.ts";
+import './ProjectCard.scss';
+import TextButton from '../TextButton/TextButton';
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Project } from '../../shared/Projects.ts';
 
 const ProjectCard = ({
   name,
@@ -11,44 +11,46 @@ const ProjectCard = ({
   year,
   role,
   technologies,
+  demo,
+  repository,
 }: Project) => {
   return (
-    <div className="project-card-container">
-      <div className="project-card-image-container">
-        <img src={image} alt="" className="project-card-image" />
+    <div className='project-card-container'>
+      <div className='project-card-image-container'>
+        <img src={image} alt='' className='project-card-image' />
       </div>
-      <div className="project-card-resume">
-        <p className="project-card-title">{name}</p>
-        <p className="project-card-text">{description}</p>
-        <div className="project-card-info-container">
-          <div className="project-card-info-row">
-            <p className="project-card-info">INFORMACIÓN DEL PROYECTO</p>
+      <div className='project-card-resume'>
+        <p className='project-card-title'>{name}</p>
+        <p className='project-card-text'>{description}</p>
+        <div className='project-card-info-container'>
+          <div className='project-card-info-row'>
+            <p className='project-card-info'>INFORMACIÓN DEL PROYECTO</p>
           </div>
-          <div className="project-card-info-row">
-            <p className="project-card-year">Año</p>
-            <p className="project-card-year-info">{year}</p>
+          <div className='project-card-info-row'>
+            <p className='project-card-year'>Año</p>
+            <p className='project-card-year-info'>{year}</p>
           </div>
-          <div className="project-card-info-row">
-            <p className="project-card-rol">Rol en el proyecto</p>
-            <p className="project-card-rol-info">{role}</p>
+          <div className='project-card-info-row'>
+            <p className='project-card-rol'>Rol en el proyecto</p>
+            <p className='project-card-rol-info'>{role}</p>
           </div>
-          <div className="project-card-info-row">
-            <p className="project-card-tech">Tecnologias</p>
-            <p className="project-card-tech-info">{technologies}</p>
+          <div className='project-card-info-row'>
+            <p className='project-card-tech'>Tecnologias</p>
+            <p className='project-card-tech-info'>{technologies}</p>
           </div>
         </div>
-        <div className="project-card-buttons">
+        <div className='project-card-buttons'>
           <TextButton
             icon={<ArrowOutwardIcon />}
-            href="https://simple-coffee-listing-pied.vercel.app"
-            text="DEMO ONLINE"
-            name=""
+            href={demo}
+            text='DEMO ONLINE'
+            name=''
           />
           <TextButton
             icon={<GitHubIcon />}
-            href="https://github.com/NahuAM1/Simple-Coffee-Listing"
-            text="REPOSITORIO DE GITHUB"
-            name=""
+            href={repository}
+            text='REPOSITORIO DE GITHUB'
+            name=''
           />
         </div>
       </div>
