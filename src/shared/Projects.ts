70bbe8bb5060fc54165fbb0682f8c80da9bb82ed
Nahuel Manero <80863427+NahuAM1@users.noreply.github.com>
2024-07-16 10:49:23 -0300

    Update Projects.ts

--- src/shared/Projects.ts
+++ src/shared/Projects.ts
@@ -11,14 +11,14 @@ const projects: Project[] = [
     {
         name: "Coffee Listing",
         description: "Este projecto es un catalago de cafés el cual consume los datos de una API y muestra los cafés populares, los puntajes dados por la gente, los disponibes, etc.",
-        image: "https://i.ibb.co/xmQPfwM/cofee-listing.png",
+        image: "https://i.postimg.cc/25z82yxw/Sin-t-tulo.png",
         year: "2024",
         role: "Desarrollador Front-End",
         technologies: "TypeScript, React, Sass",
     },{
         name: "Gym App",
         description: "Este projecto es una aplicacion que permite la gestion de clientes, pases y empleados de un gimnasio",
-        image: "https://i.ibb.co/pWjv9jZ/Sin-t-tulo.png",
+        image: "https://i.postimg.cc/DyXVsh1w/Sin-t-tulo.png",
         year: "2023",
         role: "Desarrollador Full-Stack",
         technologies: "JavaScript, React, Java-SpringBoot",
