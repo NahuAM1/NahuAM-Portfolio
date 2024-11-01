3598e65aefca79452148d9267d9a41b0caa255bd
Nahuel Manero <80863427+NahuAM1@users.noreply.github.com>
2024-11-01 13:35:30 -0300

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import Home from "./pages/home/Home";
-import AboutMe from "./pages/about-me/AboutMe";
-import NavBar from "./components/NavBar/NavBar";
-import { Routes, Route } from "react-router-dom";
-import ContactMe from "./pages/contact-me/ContactMe";
-import TopButton from "./ui-components/TopButton/TopButton";
+import Home from './pages/home/Home';
+import AboutMe from './pages/about-me/AboutMe';
+import NavBar from './components/NavBar/NavBar';
+import { Routes, Route } from 'react-router-dom';
+import ContactMe from './pages/contact-me/ContactMe';
+import TopButton from './ui-components/TopButton/TopButton';
+import Loader from './ui-components/Loader/Loader';
 
 const App = () => {
   return (
     <>
+      <Loader />
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutMe />} />
-        <Route path="/contact" element={<ContactMe />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<AboutMe />} />
+        <Route path='/contact' element={<ContactMe />} />
       </Routes>
       <TopButton />
     </>
--- src/ui-components/Loader/Loader.tsx
+++ src/ui-components/Loader/Loader.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react';
 import './Loader.scss';
+import loaderVideo from '../../shared/media/loader.mp4';
+
 const Loader = () => {
-  return <div className='loader'></div>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isLoading) return null;
+
+  return (
+    <div className='loader-container'>
+      <video autoPlay muted loop className='loader-video'>
+        <source src={loaderVideo} type='video/mp4' />
+      </video>
+    </div>
+  );
 };
 
 export default Loader;
