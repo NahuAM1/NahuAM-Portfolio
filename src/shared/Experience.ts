c38088e366280fcea09da741ab34872acd6a28bd
Nahuel Manero <80863427+NahuAM1@users.noreply.github.com>
2025-04-01 16:58:52 -0300

    Update Experience.ts

--- src/shared/Experience.ts
+++ src/shared/Experience.ts
@@ -10,10 +10,20 @@ export interface Experience {
 
 const experience: Experience[] = [
   {
+    name: 'Alkemy',
+    linkedIn: 'https://www.linkedin.com/company/alkemy2020/',
+    image:
+      'https://media.licdn.com/dms/image/v2/D4D0BAQG_ZY153logsA/company-logo_200_200/company-logo_200_200/0/1714918712803/alkemy2020_logo?e=2147483647&v=beta&t=NeBeUgZO4WlpK_6rQ9fj_iQCyCgDj3uCKcQXVe3jI2Y',
+    timeInit: 'Abril 2025',
+    timeEnd: null,
+    role: 'Desarrollador Full-Stack',
+    technologies: 'Nest JS, TypeScript, React, AWS, Auth0',
+  },
+  {
     name: 'Tech House',
     linkedIn: 'https://www.linkedin.com/company/tech-house-tech/',
     image:
-      'https://media.licdn.com/dms/image/v2/D4D0BAQG_gXI9RWqECA/company-logo_200_200/company-logo_200_200/0/1713279250515/tech_house_tech_logo?e=1742428800&v=beta&t=uDZ0hStTFM6pvvjuvB8n0OHuXKzxBORU3hQQQ1MGiwE',
+      'https://tech-house.tech/asset/logo.svg',
     timeInit: 'Noviembre 2024',
     timeEnd: 'Enero 2025',
     role: 'Pasante Desarrollador Web',
